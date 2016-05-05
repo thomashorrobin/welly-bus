86004dc0bb9e3c5c80931b1658fd4b0833f99327
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2016-05-06 09:16:50 +1200

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import stops
+import sys
+
+stops.printBusStopTimes(sys.argv[1])
+
+# print("======Args======")
+
+# for a in sys.argv[1:]:
+#     print(a)
---
+++ stops.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+def printBusStopTimes(busstop):
+	url = "https://www.metlink.org.nz/api/v1/StopDepartures/" + str(busstop)
+
+	myResponse = requests.get(url)
+
+	if(myResponse.ok):
+		jData = json.loads(myResponse.content.decode())
+
+		print(jData["Stop"]["Name"] + "(" + jData["Stop"]["Sms"] + ")")
+
+		for service in jData["Services"]:
+		    sec = int(service["DisplayDepartureSeconds"])
+		    min =  round(sec / 60)
+		    print('{0} ({2}min) {1}'.format(service["ServiceID"],service["DestinationStopName"],min))
+	else:
+	  # If response code is not ok (200), print the resulting http error code with description
+		myResponse.raise_for_status()
