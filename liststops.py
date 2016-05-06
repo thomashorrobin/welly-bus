f264ebad21c2bdd0543df7933f58ab7b751328a7
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2016-05-06 16:35:22 +1200

    added ability for stops to be listed from cmd line

--- liststops.py
+++ liststops.py
@@ -1,4 +1,15 @@
 import csv
+import prettytable
+import os.path
+
+fname = "stops.csv"
 
 def listStops():
-    print("method not yet implemented")
+    fileexists = os.path.isfile(fname)
+    if not fileexists:
+        print("You don't have any past searches")
+        return
+    f = open(fname, "r")
+    tb = prettytable.from_csv(f)
+    f.close()
+    print(tb)
