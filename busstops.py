564162b9cad58f891586bf2251f306d838c90f18
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2016-05-08 11:19:06 +1200

    messed around with the print statements to make the output more readable

--- busstops.py
+++ busstops.py
@@ -10,21 +10,23 @@ def printBusStopTimes(busstop):
 
 	if(myResponse.ok):
 		jData = json.loads(myResponse.content.decode())
-		
+
 		stopName = jData["Stop"]["Name"]
 		stopNumber = jData["Stop"]["Sms"]
 		farezone = jData["Stop"]["Farezone"]
 
-		print(stopName + "(" + stopNumber + ")")
-		
+		print("\n")
+		print(stopName)
+
 		tb = prettytable.PrettyTable(['Bus No', 'Destination', 'Est departure'])
-		
+
 		for service in jData["Services"]:
 		    departTime = divmod(int(service["DisplayDepartureSeconds"]), 60)
 		    tb.add_row([service["ServiceID"], service["DestinationStopName"], str(departTime[0]) + "mins " + str(departTime[1]) + "secs"])
-		
+
 		print(tb)
-		
+		print("\n")
+
 		liststops.addStopToList(stopNumber, stopName, farezone)
 	else:
 	  # If response code is not ok (200), print the resulting http error code with description
