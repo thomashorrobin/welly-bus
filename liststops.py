0040d7bb44e387d6730a457c1777e8675145f35a
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2016-06-25 16:44:00 +1200

    fixed up IO to put stops csv file in home directory

--- liststops.py
+++ liststops.py
@@ -2,7 +2,7 @@ import csv
 import prettytable
 import os.path
 
-fname = "~/.metlink-stops.csv"
+fname = os.path.expanduser('~') + "/.metlink-stops.csv"
 fileexists = os.path.isfile(fname)
 
 def listStops():
@@ -17,13 +17,13 @@ def listStops():
 def addStopToList(number, name, fare_zone):
     csvList = [["Stop Number","Name","Fare Zone"],[number,name,fare_zone]]
     if fileexists:
-        with open('stops.csv', 'r') as f:
+        with open(fname, 'r') as f:
             reader = csv.reader(f)
             stops_in_file_list = list(reader)
             for s in stops_in_file_list[1:]:
                 if s[0] != number:
                     csvList.append([s[0], s[1], s[2]])
-    with open('stops.csv', 'w') as f:
+    with open(fname, 'w+') as f:
         wr = csv.writer(f)
         wr.writerows(csvList)
         
